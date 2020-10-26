7660ba74e15c67b41d6796b8d55f82ff67d89384
HoangTrieuThang <valentine.tnhn@gmail.com>
2020-10-26 21:43:00 +0700

    update server-chess

---
+++ src/server/ipAddress.java
@@ -0,0 +1,23 @@
+package server;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class ipAddress {
+
+    public static String getIpAddress(){
+        InetAddress ip = null;
+
+        try {
+
+            ip = InetAddress.getLocalHost();
+            System.out.println("Current IP address : " + ip.getHostAddress());
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        return ip.getHostAddress().toString();
+    }
+
+}
